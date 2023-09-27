842ec15fc76636616f3e13c9a6a8b015c29d4b13
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2023-09-27 21:44:00 +0200

    add mobile version right menu finished

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// FUNCTION OPEN AND CLOSE MENU MOBILE
+const menuBtn = document.getElementById('menuButton')
+const rightMenu = document.querySelector('.rightMenu')
+let open = false
+function openMenu(){
+    console.log('entering open menu')
+if (open == true){
+    console.log('open is true')
+    rightMenu.classList.remove('openMenu')
+    rightMenu.classList.add('closedMenu')
+    menuBtn.classList.remove('openBtn')
+    menuBtn.classList.add('closedBtn')
+    open = false
+} else {
+    console.log('open is false')
+    rightMenu.classList.remove('closedMenu')
+    rightMenu.classList.add('openMenu')
+    menuBtn.classList.remove('closedBtn')
+    menuBtn.classList.add('openBtn')
+    open = true
+    }
+}
+menuBtn.addEventListener('click', openMenu)
+
